9244a2d17886268a7f8e22b7613676cacf75f0ee
ojbkxc <1657642978@qq.com>
2026-08-21 22:58:07 +0800

    fix(android): use isShrinkResources for AGP 8 Kotlin DSL

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         debug { isMinifyEnabled = false }
         release {
             isMinifyEnabled = true
-            shrinkResources = true
+            isShrinkResources = true
             signingConfig = signingConfigs.getByName("debug")
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
