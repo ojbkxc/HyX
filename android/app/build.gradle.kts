ef46fe941cef83c3a8aae069e0d46fd0c781604c
ojbkxc <1657642978@qq.com>
2026-08-22 09:31:04 +0800

    release: bump version to 0.2.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.ojbkxc.hyx"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2000
+        versionName = "0.2.0"
 
         ndk {
             abiFilters += listOf("arm64-v8a")
